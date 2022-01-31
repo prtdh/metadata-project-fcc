c58d94e0fd005796dfd1d0596d1355b05f7ffc2c
Preet <prtdh20@gmail.com>
2022-01-31 19:15:45 +0530

    server.js updated

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
+const multer=require('multer')
+const mongoose=require('mongoose')
+const upload = multer({ dest: 'uploads/' })
+
 
 var app = express();
+
+  
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -10,6 +16,13 @@ app.use('/public', express.static(process.cwd() + '/public'));
 app.get('/', function (req, res) {
     res.sendFile(process.cwd() + '/views/index.html');
 });
+app.post('/api/fileanalyse', upload.single('upfile'),(req, res)=> {
+    res.json({
+      "name":req.file.originalname,"type":req.file.mimetype,"size":req.file.size     
+    })
+      
+    }
+)
 
 
 
